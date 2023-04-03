150618f22a45f0e29d189ba08c08498121b0550e
SL-Pirate <isiraherath626@gmail.com>
2023-04-03 23:12:59 +0530

    Automated commit

---
+++ .deploy.sh
@@ -0,0 +1,16 @@
+src_dir="/home/slpirate/Commons/Career/Uni/assignments/WeebDev/portfolio/portfolio-src"
+deploy_dir="/home/slpirate/Commons/Career/Uni/assignments/WeebDev/portfolio/deploy"
+
+echo "cleaning deploy directory";
+rm -r $deploy_dir/*;
+cd $src_dir;
+pwd;
+flutter build web --release --web-renderer html --base-href /portfolio/;
+echo "copying built files";
+cp -r $scr_dir/build/web/* $deploy_dir;
+cd $deploy_dir;
+pwd;
+echo "commiting to github";
+git add .;
+git commit -am "Automated commit";
+git push;
