bf7c5592b0d60931e3bcb246867e7f331fd0e751
SL-Pirate <isiraherath626@gmail.com>
2023-04-03 23:26:33 +0530

    Automated commit

--- .deploy.sh
+++ .deploy.sh
@@ -4,12 +4,10 @@ deploy_dir="/home/slpirate/Commons/Career/Uni/assignments/WeebDev/portfolio/depl
 echo "cleaning deploy directory";
 rm -r $deploy_dir/*;
 cd $src_dir;
-pwd;
 flutter build web --release --web-renderer html --base-href /portfolio/;
 echo "copying built files";
 cp -r $src_dir/build/web/* $deploy_dir;
 cd $deploy_dir;
-pwd;
 echo "commiting to github";
 git add .;
 git commit -am "Automated commit";
